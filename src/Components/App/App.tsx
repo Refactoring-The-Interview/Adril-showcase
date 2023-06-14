48981de199e1b98bf7e54dac4374770ef7eab77d
sbrandon0432 <sbrandon0432@yahoo.com>
2023-06-14 12:13:47 -0400

    nav bar basic drop down function complete

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { Paths } from "../../Apis/types";
+import { DropMenu } from "../DropMenu/DropMenu";
 import { NavBar } from "../NavBar/NavBar";
 
 export const App = () => {
     return (
         <main>
             <NavBar />
+
+            <Routes>
+                <Route element={<></>} path="/" />
+                <Route element={<DropMenu />} path={Paths.dropMenu} />
+            </Routes>
         </main>
     );
 };
---
+++ src/Components/DropMenu/DropMenu.tsx
@@ -0,0 +1,9 @@
+import "./DropMenuS.scss";
+
+export const DropMenu = () => {
+    return (
+        <div className="DropMenu">
+            <h1>Drop Menu</h1>
+        </div>
+    );
+};
